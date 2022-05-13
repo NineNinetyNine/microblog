d97d0c4568a34b12fe089f6acdd34ec9829fa2d7
NineNinetyNine <anythingmax@gmail.com>
2022-05-13 09:13:59 +0100

    changed comment

--- app/routes.py
+++ app/routes.py
@@ -78,4 +78,4 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [ {'author' : user, 'body' : 'Test Post #1'}, {'author' : user, 'body' : 'Test Post #2'} ]
     return render_template('user.html', user=user, posts=posts)
-#NEXT SECTION: AVATARS
+#NEXT SECTION: AVATARS in PART 6
